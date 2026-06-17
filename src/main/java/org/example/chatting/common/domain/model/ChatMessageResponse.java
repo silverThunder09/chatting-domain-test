ba041dcfcddb9a2b14e3d5cf7edc84730a8509f0
silverThunder09 <jeg9017@gmail.com>
2026-06-17 15:57:32 +0900

    chore: ChatMessageResponse 미사용 import 제거

--- src/main/java/org/example/chatting/common/domain/model/ChatMessageResponse.java
+++ src/main/java/org/example/chatting/common/domain/model/ChatMessageResponse.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.example.chatting.common.entity.ChatMessage;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
