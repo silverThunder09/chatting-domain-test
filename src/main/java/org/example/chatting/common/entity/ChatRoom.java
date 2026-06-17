a1eb0a980ac6aa6c0d6d313f05363ae667e7e7a2
silverThunder09 <jeg9017@gmail.com>
2026-06-17 15:57:31 +0900

    feat: ChatRoom 엔티티 추가

---
+++ src/main/java/org/example/chatting/common/entity/ChatRoom.java
@@ -0,0 +1,29 @@
+package org.example.chatting.common.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public ChatRoom(String name) {
+        this.name = name;
+        this.createdAt = LocalDateTime.now();
+    }
+}
