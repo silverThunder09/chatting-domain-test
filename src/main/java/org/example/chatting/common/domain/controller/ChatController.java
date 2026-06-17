55abafab0ff60f0d9a0fc1ec2edd76b65db75b36
silverThunder09 <jeg9017@gmail.com>
2026-06-17 15:57:32 +0900

    feat: 메시지 전송 시 채팅방(roomId) 지정 및 방별 발송 처리

--- src/main/java/org/example/chatting/common/domain/controller/ChatController.java
+++ src/main/java/org/example/chatting/common/domain/controller/ChatController.java
@@ -4,8 +4,10 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.example.chatting.common.domain.model.ChatMessageDto;
 import org.example.chatting.common.domain.repository.ChatMessageRepository;
+import org.example.chatting.common.domain.repository.ChatRoomRepository;
 import org.example.chatting.common.domain.repository.UserRepository;
 import org.example.chatting.common.entity.ChatMessage;
+import org.example.chatting.common.entity.ChatRoom;
 import org.example.chatting.common.entity.User;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Controller;
 public class ChatController {
 
     private final ChatMessageRepository chatMessageRepository;
+    private final ChatRoomRepository chatRoomRepository;
     private final UserRepository userRepository;
     private final SimpMessagingTemplate messagingTemplate;
 
@@ -28,12 +31,17 @@ public class ChatController {
         User sender = userRepository
                 .findById(dto.getSenderId())
                 .orElseThrow();
-        ChatMessage message = new ChatMessage(sender, dto.getContent());
+
+        ChatRoom room = chatRoomRepository
+                .findById(dto.getRoomId())
+                .orElseThrow();
+
+        ChatMessage message = new ChatMessage(sender,room, dto.getContent());
         chatMessageRepository.save(message);
 
         // 받은 메시지를 채팅방에 발송 시켜줘야 한다
         // /sub/chat 이라는 채팅방 이름에다가
         // 외부에서 받아온 dto를 전달 시켜줄 예정이다
-        messagingTemplate.convertAndSend("/sub/chat",dto);
+        messagingTemplate.convertAndSend("/sub/chat"+dto.getRoomId() ,dto);
     }
 }
